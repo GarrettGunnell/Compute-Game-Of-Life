18bff3a339fc97acd66bdbdbcb05a963efd5b851
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-11-11 01:19:19 -0800

    Dispatch default compute shader

--- Assets/Life.cs
+++ Assets/Life.cs
@@ -6,4 +6,20 @@ public class Life : MonoBehaviour {
 
     public ComputeShader lifeCompute;
 
+    private RenderTexture target;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        if (target == null) {
+            target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            target.enableRandomWrite = true;
+            target.Create();
+        }
+
+        lifeCompute.SetTexture(0, "Result", target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        lifeCompute.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        Graphics.Blit(target, destination);
+    }
 }
