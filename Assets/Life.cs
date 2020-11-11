aadeea30376afdbf09e204741bf6619eefbcd9b5
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-11-11 13:49:12 -0800

    Parameterize chance for seeded life

--- Assets/Life.cs
+++ Assets/Life.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Life : MonoBehaviour {
 
     public ComputeShader lifeCompute;
+    
+    [Range(0.01f, 0.99f)]
+    public float seedChance;
 
     private RenderTexture target;
     private int kernel, threadGroupsX, threadGroupsY;
+
+    private void Awake() {
+        threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);     
+    }
 
     private void Seed() {
         kernel = lifeCompute.FindKernel("Seed");
         lifeCompute.SetTexture(kernel, "_Result", target);
         lifeCompute.SetFloat("_RandSeed", Random.Range(2, 1000));
+        lifeCompute.SetFloat("_SeedChance", seedChance);
         lifeCompute.Dispatch(kernel, threadGroupsX, threadGroupsY, 1);
     }
 
@@ -29,8 +38,6 @@ public class Life : MonoBehaviour {
     private void OnRenderImage(RenderTexture source, RenderTexture destination) {
         kernel = lifeCompute.FindKernel("NewGeneration");
         lifeCompute.SetTexture(kernel, "_Result", target);
-        threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
-        threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
         lifeCompute.Dispatch(kernel, threadGroupsX, threadGroupsY, 1);
 
         Graphics.Blit(target, destination);
